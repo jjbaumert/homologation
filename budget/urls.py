67f1ae225ef0f403dc7083a5ed44bc203e45b5fd
Joel Baumert <jbaumert@gmail.com>
2012-12-28 07:14:20 -0600

    Added budget item link.

--- budget/urls.py
+++ budget/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url, patterns
 
 from budget import views
 
 urlpatterns = patterns ('',
+    url(r'^([0-9]{1,4})',views.budget_item),
     url(r'^list$', views.budget_list),
 )
