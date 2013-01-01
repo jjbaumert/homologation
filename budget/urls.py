1591899cb20373b92f4d8e6eab08626fdb4d94c6
Joel Baumert <jbaumert@gmail.com>
2013-01-01 16:18:39 -0600

    down to one status function. Added a quarter section to the homologation list

--- budget/urls.py
+++ budget/urls.py
@@ -4,7 +4,8 @@ from budget import views
 
 urlpatterns = patterns ('',
     url(r'^([0-9]{1,4}$)',views.budget_item),
-    url(r'^list/(last_quarter|this_quarter|next_quarter|)$', views.budget_list),
+    url(r'^list/(last_quarter|this_quarter|next_quarter|)$', views.homologation_item_list),
+    url(r'^list/(Q)([1-4])FY(11|12|13)$', views.homologation_item_list),
 
     url(r'^([0-9]{1,4})/(requested|approved|deferred|rejected)',views.cert_status),
     url(r'^([0-9]{1,4})/(ready|submitting|in_progress|completed)',views.cert_status),
