e407b4d98794f3e92ae512afa1e3ef8d5c33283b
Joel Baumert <jbaumert@gmail.com>
2013-01-09 01:33:23 -0600

    Fixed urls with $'s to make sure there is an exact match.

--- budget/urls.py
+++ budget/urls.py
@@ -7,11 +7,11 @@ urlpatterns = patterns ('',
     url(r'^list/(last_quarter|this_quarter|next_quarter|)$', views.homologation_item_list),
     url(r'^list/(Q)([1-4])FY(11|12|13)$', views.homologation_item_list),
 
-    url(r'^([0-9]{1,4})/(requested|approved|deferred|rejected)',views.cert_status),
-    url(r'^([0-9]{1,4})/(quoting|ready|in_progress|completed|failed|cancelled)',views.cert_status),
+    url(r'^([0-9]{1,4})/(requested|approved|deferred|rejected)$',views.cert_status),
+    url(r'^([0-9]{1,4})/(quoting|ready|in_progress|completed|failed|cancelled)$',views.cert_status),
     url(r'^new$',views.item_addform),
-    url(r'^([0-9]{1,4})/edit',views.item_editform),
-    url(r'^([0-9]{1,4})/edit_amount',views.amount_form),
+    url(r'^([0-9]{1,4})/edit$',views.item_editform),
+    url(r'^([0-9]{1,4})/edit_amount$',views.amount_form),
     
     url(r'^([0-9]{1,4})/history$',views.item_history),
 )
