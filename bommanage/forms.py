425e1a40da0f25297b03b91201fde87823dcc4b7
unknown <jbaumert@MTK-VMS-ENGAP01.digi.com>
2013-04-04 07:52:10 -0500

    Added up to three characters after the dash

--- bommanage/forms.py
+++ bommanage/forms.py
@@ -2,6 +2,6 @@
 from django import forms
 
 class BomSearchForm(forms.Form):
-    part_number = forms.RegexField(regex=r'^[0-9]{5,8}(-[0-9]{1,2})?$', label='Part Number')
+    part_number = forms.RegexField(regex=r'^[0-9]{5,8}(-[0-9]{1,3})?$', label='Part Number')
 
     possibilities = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(),required=False)
